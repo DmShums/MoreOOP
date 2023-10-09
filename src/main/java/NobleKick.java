2b2e31656bc01403984e200442f3c0cfc38d763b
Dmytro Shumsky <schooldima1@gmail.com>
2023-10-09 22:11:55 +0300

    hot fix 3

--- src/main/java/NobleKick.java
+++ src/main/java/NobleKick.java
@@ -1,10 +1,11 @@
 import java.util.Random;
 
-public class NobleKick implements KickStrategy{
+public class NobleKick implements KickStrategy {
     private static Random randomGenerator = new Random();
+
     @Override
     public void kick(Character whoKick, Character opponent) {
-        int new_pow = randomGenerator.nextInt(0, whoKick.getPower());
+        int new_pow = randomGenerator.nextInt(whoKick.getPower());
         opponent.setHp(opponent.getHp() - new_pow);
     }
 }
